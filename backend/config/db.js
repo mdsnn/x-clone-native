73b61a9f1d52fc2eb26756da37c809b79136900b
mwiko <mwikosinyangwe@gmail.com>
2025-07-15 11:02:51 +0200

    project configuration

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { ENV } from "./env.js";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(ENV.MONGO_URI);
+    console.log("connected to DB succesfully");
+  } catch (error) {
+    console.log("Error connecting to MONGODB");
+    process.exit(1);
+  }
+};
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,12 @@
 import express from "express";
+import { ENV } from "../config/env.js";
+import { connectDB } from "../config/db.js";
 
 const app = express();
+connectDB();
 
-app.listen(5000, () => console.log("Server is up and running on PORT::5000"));
+app.get("/", (req, res) => res.send("Hello from the server"));
+
+app.listen(ENV.PORT, () =>
+  console.log("Server is up and running on PORT::5000")
+);
